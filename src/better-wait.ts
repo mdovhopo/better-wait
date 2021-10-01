7d3fd19fd415ae8449f209db951bba2e916c720f
glebbash <glebbash@gmail.com>
2021-10-02 00:39:12 +0300

    fix: handle 0ms duration + update function type (#1)

--- src/better-wait.ts
+++ src/better-wait.ts
@@ -1,40 +1,44 @@
 import ms from 'ms';
 
-export interface WaitOptions<T> {
-  returnValue?: T;
-  reject?: boolean;
+interface WaitFunction {
+  <T>(duration: string | number, options: { reject?: boolean; returnValue: T }): Promise<T>;
+  <T>(duration: string | number, options?: { reject?: boolean; returnValue?: T }): Promise<number>;
 }
 
-export async function wait<T = number>(
-  duration: string | number,
-  options: WaitOptions<T> = {}
-): Promise<T> {
-  let delay = duration;
-  if (typeof duration === 'string') {
-    delay = ms(duration);
-    if (!delay) {
-      throw new TypeError(
-        `[${duration}] is not a valid time format, navigate docs for format help - https://www.npmjs.com/package/ms.`
-      );
-    }
-  }
+/**
+ * Waits for specified `duration` resolving or rejecting based on options
+ * @param duration number of milliseconds or a string in `ms` format
+ * @param options promise resolution options
+ * @returns specified returnValue or wait delay in milliseconds
+ */
+export const wait: WaitFunction = async (duration, options = {}) => {
+  const delay = getDelayInMillis(duration);
+
   if (delay < 0 || delay > 2147483647) {
-    throw new TypeError(`duration must be in a range [0-2147483647].`);
+    throw new RangeError(`duration must be in a range [0-2147483647].`);
   }
+
   return new Promise((resolve, reject) => {
     const run = () => {
       const done = options.reject ? reject : resolve;
-      if (options.returnValue) {
-        return done(options.returnValue);
-      }
-      // @ts-expect-error if no returnValue is provided, T will default to number,
-      // and function will return number, ts does not like my idea though...
-      return done(delay);
+      return done(('returnValue' in options ? options.returnValue : delay) as never);
     };
-    // return on net tick instead of waiting for timers to complete
-    if (delay === 0) {
-      return run();
-    }
-    setTimeout(() => run(), delay as number);
+
+    // if delay is 0 return on next tick
+    return delay === 0 ? run() : setTimeout(() => run(), delay);
   });
+};
+
+function getDelayInMillis(duration: string | number): number {
+  if (typeof duration === 'number') {
+    return duration;
+  }
+
+  const delay = ms(duration);
+  if (delay === undefined) {
+    throw new TypeError(
+      `[${duration}] is not a valid time format, navigate docs for format help - https://www.npmjs.com/package/ms.`
+    );
+  }
+  return delay;
 }
